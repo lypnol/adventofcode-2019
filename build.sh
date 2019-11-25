fdbbb794871d50894398b924e458a90ed00126f8
Ayoub Sbai <ayoub.sbai@datadoghq.com>
2019-11-25 13:27:43 +0100

    [ci] compare to master

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 
 set -ev
 
-git --no-pager diff --name-only master -- | grep "day-" | cut -d "/" -f1 | cut -d "-" -f2 | sort | uniq | xargs -I{} ./aoc run -fd {}
+git --no-pager diff --name-only origin/master -- | grep "day-" | cut -d "/" -f1 | cut -d "-" -f2 | sort | uniq | xargs -I{} ./aoc run -fd {}
