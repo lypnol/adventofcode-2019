3aeb31f85cd3f080f42d5dc919e6dfbc7e4bcb4b
Aimery Tauveron <atauveron@via.ecp.fr>
2019-12-06 22:56:01 +0100

    Day 6 part 2, fast algorithm

--- day-06/part-2/atauveron.rs
+++ day-06/part-2/atauveron.rs
@@ -1,7 +1,7 @@
 use std::env::args;
 use std::time::Instant;
 
-use std::collections::{HashMap, HashSet};
+use std::collections::{HashMap, LinkedList};
 
 fn main() {
     let now = Instant::now();
@@ -12,64 +12,37 @@ fn main() {
 }
 
 fn run(input: &str) -> usize {
-    // Map of direct orbits (key: center; value: objects in orbit around it)
-    let mut neighbours: HashMap<String, HashSet<String>> = HashMap::new();
+    // Map of direct orbits (key: objects in orbit; value: center)
+    let mut parents: HashMap<String, String> = HashMap::new();
     for line in input.lines() {
         let mut it = line.split(')');
         let center = it.next().unwrap();
         let obj = it.next().unwrap();
-        if neighbours.contains_key(center) {
-            neighbours.get_mut(center).unwrap().insert(obj.to_string());
-        } else {
-            let mut tmp = HashSet::new();
-            tmp.insert(obj.to_string());
-            neighbours.insert(center.to_string(), tmp);
-        }
-        if neighbours.contains_key(obj) {
-            neighbours.get_mut(obj).unwrap().insert(center.to_string());
-        } else {
-            let mut tmp = HashSet::new();
-            tmp.insert(center.to_string());
-            neighbours.insert(obj.to_string(), tmp);
-        }
+        parents.insert(obj.to_string(), center.to_string());
     }
+    let you = ancestors("YOU", &parents);
+    let santa = ancestors("SAN", &parents);
     // Compute the distance
-    // Minus two  for the first and last jumps
-    dijkstra("YOU", "SAN", &neighbours) - 2
+    let mut common: usize = 0;
+    let mut it_you = you.iter();
+    let mut it_santa = santa.iter();
+    while it_you.next() == it_santa.next() {
+        common += 1
+    }
+    // Minus two for the first and last jumps
+    you.len() + santa.len() - 2 * common
 }
 
-fn dijkstra(origin: &str, dest: &str, graph: &HashMap<String, HashSet<String>>) -> usize {
-    // Initialise
-    let mut distances: HashMap<String, usize> = HashMap::new();
-    for vertex in graph.keys() {
-        distances.insert(vertex.to_string(), usize::max_value());
-    }
-    *distances.get_mut(origin).unwrap() = 0;
-
-    let mut to_visit: HashSet<String> = HashSet::new();
-    for vertex in graph.keys() {
-        to_visit.insert(vertex.to_string());
-    }
-    // Loop
-    while !to_visit.is_empty() {
-        let mut min_loc = usize::max_value();
-        let mut selected = String::new();
-        for vertex in &to_visit {
-            if distances.get(vertex).unwrap() < &min_loc {
-                min_loc = *distances.get(vertex).unwrap();
-                selected = vertex.to_string();
-            }
-        }
-        to_visit.remove(&selected);
-        let new_dist = distances.get(&selected).unwrap() + 1;
-        for vertex in graph.get(&selected).unwrap().intersection(&to_visit) {
-            if new_dist < *distances.get(vertex).unwrap() {
-                *distances.get_mut(vertex).unwrap() = new_dist;
-            }
+fn ancestors(object: &str, parents: &HashMap<String, String>) -> LinkedList<String> {
+    match object {
+        "COM" => LinkedList::new(),
+        _ => {
+            let parent = parents.get(object).unwrap();
+            let mut tmp = ancestors(parent, parents);
+            tmp.push_back(parent.to_string());
+            tmp
         }
     }
-    // Return
-    *distances.get(dest).unwrap()
 }
 
 #[cfg(test)]
