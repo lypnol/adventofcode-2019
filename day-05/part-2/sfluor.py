133c83c6fb6286050df0f17e223a20f79cb72f47
Sami Tabet <salph.tabet@gmail.com>
2019-12-07 11:18:13 +0100

    sfluor: refactor d5

--- day-05/part-2/sfluor.py
+++ day-05/part-2/sfluor.py
@@ -1,89 +1,82 @@
 import itertools
+import itertools
 from tool.runners.python import SubmissionPy
 
 
-def op_add(prog, args):
-    prog[args[2]] = args[1] + args[0]
+class Program(object):
+    def __init__(self, code, inp):
+        self.inp = inp
+        self.out = []
+        self.code = code
 
+    def op_add(self, args):
+        self.code[args[2]] = args[1] + args[0]
 
-def op_mul(prog, args):
-    prog[args[2]] = args[1] * args[0]
+    def op_mul(self, args):
+        self.code[args[2]] = args[1] * args[0]
 
+    def op_inp(self, args):
+        self.code[args[0]] = self.inp.pop(0)
 
-def op_inp(prog, args):
-    prog[args[0]] = 5  # fake input
+    def op_out(self, args):
+        self.out.append(args[0])
 
+    def jump_if_true(self, args):
+        if args[0] != 0:
+            return args[1]
 
-def op_out(prog, args):
-    # store the output at the end of the program
-    prog[-1] = args[0]
+    def jump_if_false(self, args):
+        if args[0] == 0:
+            return args[1]
 
+    def less_than(self, args):
+        self.code[args[2]] = int(args[0] < args[1])
 
-def jump_if_true(prog, args):
-    if args[0] != 0:
-        return args[1]
+    def equals(self, args):
+        self.code[args[2]] = int(args[0] == args[1])
 
+    def params(self, pc, modes, pos=None):
+        # overkill to avoid recomputing 1, 10, 100
+        for i, mask in enumerate([1, 10, 100]):
+            p = self.code[pc + i + 1]
+            if modes & mask or pos == i + 1:
+                yield p
+            else:
+                yield self.code[p]
 
-def jump_if_false(prog, args):
-    if args[0] == 0:
-        return args[1]
+    def run(self):
+        # operands, func, optional non immediate arg
+        opcodes = [
+            (3, self.op_add, 3),
+            (3, self.op_mul, 3),
+            (1, self.op_inp, 1),
+            (1, self.op_out, None),
+            (2, self.jump_if_true, None),
+            (2, self.jump_if_false, None),
+            (3, self.less_than, 3),
+            (3, self.equals, 3),
+        ]
+        STOP = 99
 
+        pc = 0
 
-def less_than(prog, args):
-    prog[args[2]] = int(args[0] < args[1])
+        while True:
+            opcode = self.code[pc]
 
+            if opcode == STOP:
+                return self.out[-1]
 
-def equals(prog, args):
-    prog[args[2]] = int(args[0] == args[1])
+            n_args, func, pos = opcodes[(opcode % 100) - 1]
 
+            args = list(
+                itertools.islice(self.params(pc, opcode // 100, pos=pos), n_args)
+            )
 
-def resolve_params(prog, pc, modes, pos=None):
-    # overkill to avoid recomputing 1, 10, 100
-    for i, mask in enumerate([1, 10, 100]):
-        p = prog[pc + i + 1]
-        if modes & mask or pos == i + 1:
-            yield p
-        else:
-            yield prog[p]
-
-
-# operands, func, optional non immediate arg
-opcodes = [
-    (3, op_add, 3),
-    (3, op_mul, 3),
-    (1, op_inp, 1),
-    (1, op_out, None),
-    (2, jump_if_true, None),
-    (2, jump_if_false, None),
-    (3, less_than, 3),
-    (3, equals, 3),
-]
-STOP = 99
-
-
-def exec_prog(prog):
-    # Add one element for the output
-    prog.append(None)
-
-    pc = 0
-
-    while True:
-        opcode = prog[pc]
-
-        if opcode == STOP:
-            return prog[-1]
-
-        n_args, func, pos = opcodes[(opcode % 100) - 1]
-
-        args = list(
-            itertools.islice(resolve_params(prog, pc, opcode // 100, pos=pos), n_args)
-        )
-
-        new_pc = func(prog, args)
-        if new_pc is not None:
-            pc = new_pc
-        else:
-            pc += n_args + 1
+            new_pc = func(args)
+            if new_pc is not None:
+                pc = new_pc
+            else:
+                pc += n_args + 1
 
 
 class SfluorSubmission(SubmissionPy):
@@ -91,5 +84,5 @@ class SfluorSubmission(SubmissionPy):
         # :param s: input in string format
         # :return: solution flag
         # Your code goes here
-        prog = [int(i) for i in s.split(",")]
-        return exec_prog(prog)
+        code = [int(i) for i in s.split(",")]
+        return Program(code, [5]).run()
