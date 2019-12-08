22301fc14da1034cd7d57b3baaf1a2df062689f5
lithium <louis.cabrol@student.ecp.fr>
2019-12-08 13:56:04 +0100

    feat: fast d3p1

--- day-03/part-1/lithium.c
+++ day-03/part-1/lithium.c
@@ -3,13 +3,32 @@
 #include <stdlib.h>
 #include <string.h>
 
-int coords(int x, int y){
-    return (40000 * (x + 20000)) + (y + 20000);
-}
+struct segment {
+    int start;
+    int end;
+    int offset;
+    // int initial_distance; // shh, part 2
+};
+typedef struct segment segment;
 
 int run(char* str){
-    int *space;
-    space = (int *)malloc(sizeof(int)*40000*40000);
+    segment *hor_segments_1;
+    segment *ver_segments_1;
+
+    hor_segments_1 = (segment *)malloc(sizeof(segment)*200);
+    ver_segments_1 = (segment *)malloc(sizeof(segment)*200);
+
+    segment *hor_segments_2;
+    segment *ver_segments_2;
+
+    hor_segments_2 = (segment *)malloc(sizeof(segment)*200);
+    ver_segments_2 = (segment *)malloc(sizeof(segment)*200);
+
+    int hor_index_1 = 0;
+    int ver_index_1 = 0;
+
+    int hor_index_2 = 0;
+    int ver_index_2 = 0;
 
     int last_x = 0;
     int last_y = 0;
@@ -22,40 +41,44 @@ int run(char* str){
 
     char *token_line_1 = strtok(line1, token_separator);
 
-    int modx = 0;
-    int mody = 0;
-
     int best = __INT_MAX__;
+    segment tmp;
 
     while (token_line_1 != NULL) {
         int value = atoi(token_line_1 + 1);
         switch(token_line_1[0]){
             case 'R':{
-                modx = 1;
-                mody = 0;
+                tmp.start = last_x;
+                last_x += value;
+                tmp.end = last_x;
+                tmp.offset = last_y;
+                hor_segments_1[hor_index_1++] = tmp;
                 break;
             }
             case 'L':{
-                modx = -1;
-                mody = 0;
+                tmp.end = last_x;
+                last_x -= value;
+                tmp.start = last_x;
+                tmp.offset = last_y;
+                hor_segments_1[hor_index_1++] = tmp;
                 break;
             }
             case 'U':{
-                modx = 0;
-                mody = 1;
+                tmp.start = last_y;
+                last_y += value;
+                tmp.end = last_y;
+                tmp.offset = last_x;
+                ver_segments_1[ver_index_1++] = tmp;
                 break;
             }
             case 'D':{
-                modx = 0;
-                mody = -1;
+                tmp.end = last_y;
+                last_y -= value;
+                tmp.start = last_y;
+                tmp.offset = last_x;
+                ver_segments_1[ver_index_1++] = tmp;
                 break;
             }
-        }
-        for(int i = 0; i < value; i++){
-            last_x += modx;
-            last_y += mody;
-            //printf("(%d:%d)\n", last_x, last_y);
-            space[coords(last_x, last_y)] = 1;
         }
         token_line_1 = strtok(NULL, token_separator);
     }
@@ -63,45 +86,82 @@ int run(char* str){
     last_y = 0;
 
     char *token_line_2 = strtok(line2, token_separator);
+
     while (token_line_2 != NULL) {
         int value = atoi(token_line_2 + 1);
         switch(token_line_2[0]){
             case 'R':{
-                modx = 1;
-                mody = 0;
+                tmp.start = last_x;
+                last_x += value;
+                tmp.end = last_x;
+                tmp.offset = last_y;
+                hor_segments_2[hor_index_2++] = tmp;
                 break;
             }
             case 'L':{
-                modx = -1;
-                mody = 0;
+                tmp.end = last_x;
+                last_x -= value;
+                tmp.start = last_x;
+                tmp.offset = last_y;
+                hor_segments_2[hor_index_2++] = tmp;
                 break;
             }
             case 'U':{
-                modx = 0;
-                mody = 1;
+                tmp.start = last_y;
+                last_y += value;
+                tmp.end = last_y;
+                tmp.offset = last_x;
+                ver_segments_2[ver_index_2++] = tmp;
                 break;
             }
             case 'D':{
-                modx = 0;
-                mody = -1;
+                tmp.end = last_y;
+                last_y -= value;
+                tmp.start = last_y;
+                tmp.offset = last_x;
+                ver_segments_2[ver_index_2++] = tmp;
                 break;
             }
-        }
-        for(int i = 0; i < value; i++){
-            last_x += modx;
-            last_y += mody;
-            if (space[coords(last_x, last_y)] == 1){
-                int distance = abs(last_x) + abs(last_y);
-                if (distance < best){
-                    best = distance;
-                }
-            }
-            //printf("(%d:%d)\n", last_x-5000,last_y-5000);
         }
         token_line_2 = strtok(NULL, token_separator);
     }
 
-    free(space);
+    for (int hor_1 = 0; hor_1 < hor_index_1; hor_1++){
+        for (int ver_2 = 0; ver_2 < ver_index_2; ver_2++){
+            if 
+            (
+                hor_segments_1[hor_1].start <= ver_segments_2[ver_2].offset &&
+                hor_segments_1[hor_1].end >= ver_segments_2[ver_2].offset &&
+                ver_segments_2[ver_2].start <= hor_segments_1[hor_1].offset &&
+                ver_segments_2[ver_2].end >= hor_segments_1[hor_1].offset
+            )
+            {
+                if (abs(ver_segments_2[ver_2].offset) + abs(hor_segments_1[hor_1].offset) < best){
+                    best = abs(ver_segments_2[ver_2].offset) + abs(hor_segments_1[hor_1].offset);
+                }
+            }
+        }
+    }
+    for (int hor_2 = 0; hor_2 < hor_index_2; hor_2++){
+        for (int ver_1 = 0; ver_1 < ver_index_1; ver_1++){
+            if 
+            (
+                hor_segments_2[hor_2].start <= ver_segments_1[ver_1].offset &&
+                hor_segments_2[hor_2].end >= ver_segments_1[ver_1].offset &&
+                ver_segments_1[ver_1].start <= hor_segments_2[hor_2].offset &&
+                ver_segments_1[ver_1].end >= hor_segments_2[hor_2].offset
+            )
+            {
+                if (abs(hor_segments_2[hor_2].offset) + abs(ver_segments_1[ver_1].offset) < best){
+                    best = abs(ver_segments_1[ver_1].offset) + abs(hor_segments_2[hor_2].offset);
+                }
+            }
+        }
+    }
+    free(hor_segments_1);
+    free(hor_segments_2);
+    free(ver_segments_1);
+    free(ver_segments_2);
     return best;
 }
 
