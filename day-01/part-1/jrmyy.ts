b20e67000c6e4be1fc8306b29161cba66c358ffc
Jrmyy <guiselin.j@gmail.com>
2019-12-08 12:15:15 +0100

    Add jrmyy day 8 + fix tslint

--- day-01/part-1/jrmyy.ts
+++ day-01/part-1/jrmyy.ts
@@ -1,25 +1,25 @@
 class Solution {
 
-	// This is your puzzle input
-	private readonly input: string;
+    // This is your puzzle input
+    private readonly input: string;
 
-	constructor(input: string) {
-		this.input = input;
-	}
+    constructor(input: string) {
+        this.input = input;
+    }
 
-	run(): number {
-		return this.input.split('\n')
-			.map(v => Math.floor(parseInt(v, 10) / 3) - 2)
-			.reduce((acc, value) => acc + value, 0);
-	}
+    run(): number {
+        return this.input.split('\n')
+            .map(v => Math.floor(parseInt(v, 10) / 3) - 2)
+            .reduce((acc, value) => acc + value, 0);
+    }
 
-	main() {
-		const start = Date.now();
-		const answer = this.run();
+    main() {
+        const start = Date.now();
+        const answer = this.run();
 
-		console.log(`_duration:${(Date.now() - start).toString()}`);
-		console.log(answer);
-	}
+        console.log(`_duration:${(Date.now() - start).toString()}`);
+        console.log(answer);
+    }
 }
 
 // @ts-ignore
