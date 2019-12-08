49c2ed36d6c9b45fd2fcdee6219c30f0f243698f
Thomas Renault <thomas.renault56@gmail.com>
2019-12-08 11:28:36 +0100

    thoe day 8

---
+++ day-08/part-2/thore.py
@@ -0,0 +1,27 @@
+from textwrap import wrap
+
+from tool.runners.python import SubmissionPy
+
+
+class ThoreSubmission(SubmissionPy):
+    def run(self, s):
+        # :param s: input in string format
+        # :return: solution flag
+        LAYER_WIDTH = 25
+        LAYER_HEIGHT = 6
+
+        layers = wrap(s, LAYER_WIDTH * LAYER_HEIGHT)
+
+        image = [[2 for j in range(LAYER_WIDTH)] for i in range(LAYER_HEIGHT)]
+        for i in range(LAYER_HEIGHT):
+            for j in range(LAYER_WIDTH):
+                idx = i * LAYER_WIDTH + j
+                for k in range(len(layers)):
+                    if layers[k][idx] != "2":
+                        image[i][j] = layers[k][idx]
+                        break
+
+        lines = ["".join(line) for line in image]
+        image_str = "\n".join(lines).replace("0", u"\u2588").replace("1", " ")
+        print(image_str)
+        return
