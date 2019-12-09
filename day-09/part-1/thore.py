179c39d1ae3fa4d4382d2dadbe182f3945ef002e
Thomas Renault <thomas.renault56@gmail.com>
2019-12-09 13:10:27 +0100

    refactor vm

--- day-09/part-1/thore.py
+++ day-09/part-1/thore.py
@@ -31,70 +31,44 @@ class VM:
         self.is_halted = False
         self.is_waiting_for_input = False
 
+        self.opcodes = {
+            1: (self.op_add, 3),
+            2: (self.op_mul, 3),
+            3: (self.op_input, 1),
+            4: (self.op_output, 1),
+            5: (self.op_jump_if_true, 2),
+            6: (self.op_jump_if_false, 2),
+            7: (self.op_lt, 3),
+            8: (self.op_eq, 3),
+            9: (self.op_rel_base, 1),
+            99: (self.op_halt, 0),
+        }
+
+    def add_input(self, value):
+        self.stdin.append(value)
+        self.is_waiting_for_input = False
+
+    def get_output(self):
+        if len(self.stdout) == 0:
+            return None
+        return self.stdout.popleft()
+
+    def run(self):
+        while not self.is_halted and not self.is_waiting_for_input:
+            self.step()
+
     def step(self):
         instruction = self.memory[self.pc]
         opcode = instruction % 100
 
-        if opcode == 1:  # ADD
-            n_params = 3
-            a_p, b_p, dest_p = self.get_params_pointers(instruction, n_params)
-            self.memory[dest_p] = self.memory[a_p] + self.memory[b_p]
-            self.pc += 1 + n_params
-        elif opcode == 2:  # MUL
-            n_params = 3
-            a_p, b_p, dest_p = self.get_params_pointers(instruction, n_params)
-            self.memory[dest_p] = self.memory[a_p] * self.memory[b_p]
-            self.pc += 1 + n_params
-        elif opcode == 3:  # IN
-            n_params = 1
-            dest_p, = self.get_params_pointers(instruction, n_params)
-            if len(self.stdin) == 0:
-                self.is_waiting_for_input = True
-                return
-            self.is_waiting_for_input = False
-            value = self.stdin.popleft()
-            self.memory[dest_p] = value
-            self.pc += 1 + n_params
-        elif opcode == 4:  # OUT
-            n_params = 1
-            a_p, = self.get_params_pointers(instruction, 1)
-            value = self.memory[a_p]
-            self.stdout.append(value)
-            self.pc += 1 + n_params
-        elif opcode == 5:  # JUMP-IF-TRUE
-            n_params = 2
-            cond_p, value_p = self.get_params_pointers(instruction, 2)
-            if self.memory[cond_p] != 0:
-                self.pc = self.memory[value_p]
-            else:
-                self.pc += 1 + n_params
-        elif opcode == 6:  # JUMP-IF-FALSE
-            n_params = 2
-            cond_p, value_p = self.get_params_pointers(instruction, 2)
-            if self.memory[cond_p] == 0:
-                self.pc = self.memory[value_p]
-            else:
-                self.pc += 1 + n_params
-        elif opcode == 7:  # LT
-            n_params = 3
-            a_p, b_p, dest_p = self.get_params_pointers(instruction, n_params)
-            self.memory[dest_p] = int(self.memory[a_p] < self.memory[b_p])
-            self.pc += 1 + n_params
-        elif opcode == 8:  # EQ
-            n_params = 3
-            a_p, b_p, dest_p = self.get_params_pointers(instruction, n_params)
-            self.memory[dest_p] = int(self.memory[a_p] == self.memory[b_p])
-            self.pc += 1 + n_params
-        elif opcode == 9:  # REL_BASE
-            n_params = 1
-            val_p, = self.get_params_pointers(instruction, n_params)
-            self.relative_base += self.memory[val_p]
-            self.pc += 1 + n_params
-        elif opcode == 99:  # HALT
-            self.is_halted = True
-            return
-        else:
+        if opcode not in self.opcodes:
             raise f"Invalid opcode: {opcode}"
+
+        op_func, n_params = self.opcodes[opcode]
+        params = self.get_params_pointers(instruction, n_params)
+        pc = op_func(*params)
+        if pc is not None:
+            self.pc = pc
 
         return
 
@@ -112,15 +86,51 @@ class VM:
         else:
             raise ValueError(f"Unknown parameter mode {mode}")
 
-    def run(self):
-        while not self.is_halted and not self.is_waiting_for_input:
-            self.step()
+    def op_add(self, a_p, b_p, dest_p):
+        self.memory[dest_p] = self.memory[a_p] + self.memory[b_p]
+        return self.pc + 4
 
-    def add_input(self, value):
-        self.stdin.append(value)
-        self.is_waiting_for_input = False
+    def op_mul(self, a_p, b_p, dest_p):
+        self.memory[dest_p] = self.memory[a_p] * self.memory[b_p]
+        return self.pc + 4
 
-    def get_output(self):
-        if len(self.stdout) == 0:
+    def op_input(self, dest_p):
+        if len(self.stdin) == 0:
+            self.is_waiting_for_input = True
             return None
-        return self.stdout.popleft()
+        value = self.stdin.popleft()
+        self.memory[dest_p] = value
+        return self.pc + 2
+
+    def op_output(self, a_p):
+        value = self.memory[a_p]
+        self.stdout.append(value)
+        return self.pc + 2
+
+    def op_jump_if_true(self, cond_p, value_p):
+        if self.memory[cond_p] != 0:
+            return self.memory[value_p]
+        else:
+            return self.pc + 3
+
+    def op_jump_if_false(self, cond_p, value_p):
+        if self.memory[cond_p] == 0:
+            return self.memory[value_p]
+        else:
+            return self.pc + 3
+
+    def op_lt(self, a_p, b_p, dest_p):
+        self.memory[dest_p] = int(self.memory[a_p] < self.memory[b_p])
+        return self.pc + 4
+
+    def op_eq(self, a_p, b_p, dest_p):
+        self.memory[dest_p] = int(self.memory[a_p] == self.memory[b_p])
+        return self.pc + 4
+
+    def op_rel_base(self, val_p):
+        self.relative_base += self.memory[val_p]
+        return self.pc + 2
+
+    def op_halt(self):
+        self.is_halted = True
+        return None
