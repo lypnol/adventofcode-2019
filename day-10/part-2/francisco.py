fac0d75441c03558f02cfd5ee3bb68a4378689a9
Francisco-Pierre Puig <puig.franciscopierre@gmail.com>
2019-12-11 11:35:05 +0100

    francisco: faster day 10

--- day-10/part-2/francisco.py
+++ day-10/part-2/francisco.py
@@ -5,7 +5,12 @@ import math
 
 
 def parse_grid(s):
-    return [[char == "#" for char in line.strip()] for line in s.splitlines()]
+    return [
+        (x, y)
+        for x, line in enumerate(s.splitlines())
+        for y, char in enumerate(line.strip())
+        if char == "#"
+    ]
 
 
 def direction(x1, y1, x2, y2):
@@ -17,47 +22,43 @@ def direction(x1, y1, x2, y2):
 
 def seeable(grid, x1, y1):
     directions = set()
-    for x2 in range(len(grid)):
-        for y2 in range(len(grid[0])):
-            if (x1, y1) != (x2, y2) and grid[x2][y2]:
-                directions.add(direction(x1, y1, x2, y2))
+    for (x2, y2) in grid:
+        if (x1, y1) != (x2, y2):
+            directions.add(direction(x1, y1, x2, y2))
     return len(directions)
 
 
 def solve_part1(grid):
     return max(
-        ((x, y)
-        for x in range(len(grid))
-        for y in range(len(grid[0]))
-        if grid[x][y]),
-        key = lambda pos: seeable(grid, pos[0], pos[1])
+        ((x, y) for (x, y) in grid), key=lambda pos: seeable(grid, pos[0], pos[1]),
     )
+
 
 def angle(x, y):
     # an angle is made of 2 parts
-    # - the quadrant of the place it's in (between 1 and 4)
+    # - the quadrant of the plane it's in (between 1 and 4)
     # - the angle itself, between 0 and infinite
     if x >= 0 and y > 0:
-        return (1, abs(x/y))
+        return (1, abs(x / y))
     elif x > 0 and y <= 0:
-        return (2, abs(y/x))
+        return (2, abs(y / x))
     elif x <= 0 and y < 0:
-        return (3, abs(x/y))
+        return (3, abs(x / y))
     elif x < 0 and y >= 0:
-        return (4, abs(y/x))
+        return (4, abs(y / x))
+
 
 def sorted_asteroids(grid, x1, y1):
     asteroids = dict()
 
-    for x2 in range(len(grid)):
-        for y2 in range(len(grid[0])):
-            if (x1, y1) != (x2, y2) and grid[x2][y2]:
-                x, y = x2-x1, y2-y1
-                a = angle(y, -x)
-                a = (a[0], int(a[1] * 10e10))
-                if a not in asteroids:
-                    asteroids[a] = []
-                asteroids[a].append((abs(x2-x1)+abs(y2-y1), (x2, y2)))
+    for (x2, y2) in grid:
+        if (x1, y1) != (x2, y2):
+            x, y = x2 - x1, y2 - y1
+            a = angle(y, -x)
+            a = (a[0], int(a[1] * 10e10))
+            if a not in asteroids:
+                asteroids[a] = []
+            asteroids[a].append((abs(x) + abs(y), (x2, y2)))
 
     for l in asteroids.values():
         l.sort()
@@ -65,19 +66,19 @@ def sorted_asteroids(grid, x1, y1):
 
     return asteroids
 
+
 def solve_part2(grid, x, y):
     asteroids = sorted_asteroids(grid, x, y)
     angles = sorted(asteroids)
-    i = 1
     popped = []
     while max(len(l) for l in asteroids.values()) > 0:
         for a in angles:
             if len(asteroids[a]) > 0:
                 dist, coords = asteroids[a].pop()
                 popped.append(coords)
-                i += 1
 
     return popped
+
 
 class FranciscoSubmission(SubmissionPy):
     def run(self, s):
