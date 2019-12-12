cde6a5d5302bbbf766f4f18b2705d189e184fdf7
Thomas P <tpxp@live.fr>
2019-12-12 17:21:28 +0100

    FIX TPXP's implementation for part 1 of day 10

--- day-10/part-1/tpxp.rb
+++ day-10/part-1/tpxp.rb
@@ -1,5 +1,5 @@
 def reduce(dx,dy)
-    l = [dx, dy].map(&:abs).reject{ |n| n == 0 }.min
+    l = dx.gcd(dy)
     if dx % l == 0 and dy % l == 0
         dx /= l
         dy /= l
@@ -15,36 +15,34 @@ def how_many_visible_from(x, y, map)
     visible = 0
     (1...r_max).each do |i|
         # puts ["q", i, visible].inspect
-        [-i, +i].each do |dx|
-            (-i..+i).each do |dy|
-                nx = x + dx
-                ny = y + dy
+        [-i, +i].each do |gdx|
+            (-i..+i).each do |gdy|
+                nx = x + gdx
+                ny = y + gdy
                 # Out of bounds
                 next if nx < 0 or ny < 0
                 next if map[nx] == nil or map[nx][ny] == nil
-                # Make it irreductible
-                dx, dy = reduce(dx, dy)
-                #puts [nx, ny].inspect
-                next if blocked[dx][dy]
                 next if map[nx][ny] != '#'
+                # Make it irreductible
+                # Careful, if you set gdx here it will be used for the rest!
+                dx, dy = reduce(gdx, gdy)
+                next if blocked[dx][dy]
                 blocked[dx][dy] = true
                 visible += 1
             end
         end
-        #puts "next"
-        # So basically this is a copy paste from the above but with dx and dy inverted
-        [-i, +i].each do |dy|
-            (-i..+i).each do |dx|
-                nx = x + dx
-                ny = y + dy
+        # So basically this is a copy paste from the above but with gdx and gdy inverted
+        [-i, +i].each do |gdy|
+            (-i..+i).each do |gdx|
+                nx = x + gdx
+                ny = y + gdy
                 # Out of bounds
                 next if nx < 0 or ny < 0
                 next if map[nx] == nil or map[nx][ny] == nil
-                # Make it irreductible
-                dx, dy = reduce(dx, dy)
-                #puts [nx, ny].inspect
-                next if blocked[dx][dy]
                 next if map[nx][ny] != '#'
+                # Make it irreductible
+                dx, dy = reduce(gdx, gdy)
+                next if blocked[dx][dy]
                 blocked[dx][dy] = true
                 visible += 1
             end
@@ -54,13 +52,12 @@ def how_many_visible_from(x, y, map)
 end
 
 def run(map)
-    #puts 'NEW RUN'
+    # Sanitization: remove the line break at the end of lines
+    map = map.map &:strip
     v = 0
     map.length.times do |x|
         map[x].length.times do |y|
             next if map[x][y] == '.'
-            #puts 'yo'
-            #puts [x,y].inspect
             n = how_many_visible_from(x, y, map)
             v = n if n > v
         end
