6e9feb9b14973cee126527bdddd8ba7ba1cedf76
Jonathan Remy <jonremy@users.noreply.github.com>
2019-12-14 01:55:16 +0100

    Faster intcode runner

--- tool/runners/intcode.py
+++ tool/runners/intcode.py
@@ -1,3 +1,4 @@
+import collections
 
 
 class SubmissionIntcode:
@@ -20,60 +21,50 @@ class SubmissionIntcode:
 
 
 def compute(code, inputs):
-    mem = {}
+    p = collections.defaultdict(int, enumerate(code))
     pc = 0
     relative_base = 0
 
-    def p(i):
-        if i in mem:
-            return mem[i]
-        if i < len(code):
-            return code[i]
-        raise Exception("Cannot read index {}, pc={}".format(i, pc))
-
     def index(pos):
-        mode = (p(pc) // 10 ** (1 + pos)) % 10
+        mode = (p[pc] // 10 ** (1 + pos)) % 10
         if mode == 0:
-            return p(pc + pos)
+            return p[pc + pos]
         if mode == 1:
             return pc + pos
         if mode == 2:
-            return relative_base + p(pc + pos)
+            return relative_base + p[pc + pos]
         raise Exception("Bad param mode")
-
-    def param(pos):
-        return p(index(pos))
 
     input_index = 0
 
     while True:
-        op = p(pc) % 100
+        op = p[pc] % 100
 
         if op == 1:
-            mem[index(3)] = param(1) + param(2)
+            p[index(3)] = p[index(1)] + p[index(2)]
             pc += 4
         elif op == 2:
-            mem[index(3)] = param(1) * param(2)
+            p[index(3)] = p[index(1)] * p[index(2)]
             pc += 4
         elif op == 3:
-            mem[index(1)] = inputs[input_index]
+            p[index(1)] = inputs[input_index]
             input_index += 1
             pc += 2
         elif op == 4:
-            yield param(1)
+            yield p[index(1)]
             pc += 2
         elif op == 5:
-            pc = param(2) if param(1) != 0 else pc + 3
+            pc = p[index(2)] if p[index(1)] != 0 else pc + 3
         elif op == 6:
-            pc = param(2) if param(1) == 0 else pc + 3
+            pc = p[index(2)] if p[index(1)] == 0 else pc + 3
         elif op == 7:
-            mem[index(3)] = 1 if param(1) < param(2) else 0
+            p[index(3)] = 1 if p[index(1)] < p[index(2)] else 0
             pc += 4
         elif op == 8:
-            mem[index(3)] = 1 if param(1) == param(2) else 0
+            p[index(3)] = 1 if p[index(1)] == p[index(2)] else 0
             pc += 4
         elif op == 9:
-            relative_base += param(1)
+            relative_base += p[index(1)]
             pc += 2
         elif op == 99:
             break
