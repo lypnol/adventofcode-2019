880fa92adf4d0a9cc6119a56fe2b2588f47cc059
Rémi Calixte <remicalixte.rmc@gmail.com>
2019-12-16 20:29:31 +0100

    speedup a bit

--- day-16/part-2/remi.py
+++ day-16/part-2/remi.py
@@ -1,14 +1,31 @@
 from tool.runners.python import SubmissionPy
 import itertools
+import numpy as np
 
 
 class RemiSubmission(SubmissionPy):
     def run(self, s):
-        inp = [int(i) for i in list(s)] * 10000
-        self.offset = int("".join(str(i) for i in inp[:7]))
-        inp = inp[self.offset :]
-        for _ in range(100):
-            for i in range(len(inp) - 2, -1, -1):
-                inp[i] += inp[i + 1]
+        offset = int(s[:7])
+        inp = np.array([int(i) for i in list(s)])
+        inp1 = inp[offset % len(s) :]
+        inp = np.flip(
+            np.array(
+                [
+                    i
+                    for i in itertools.islice(
+                        itertools.chain(inp1, itertools.cycle(inp)),
+                        0,
+                        len(s) * 10000 - offset,
+                    )
+                ],
+                dtype=np.uint64,
+            )
+        )
+        for j in range(100):
+            np.cumsum(inp, out=inp)
+            if j % 3 == 0:
+                np.mod(inp, 10, out=inp)
 
-        return "".join([str(i % 10) for i in inp[:8]])
+        inp = np.flip(inp, axis=0)
+
+        return "".join([str(i) for i in inp[:8]])
