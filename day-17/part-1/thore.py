cfb63b14044f1eb77a75515223d5729fa9a0aa26
Thomas Renault <thomas.renault56@gmail.com>
2019-12-17 09:00:59 +0100

    day 17 part 1

---
+++ day-17/part-1/thore.py
@@ -0,0 +1,168 @@
+from collections import defaultdict, deque
+
+from tool.runners.python import SubmissionPy
+
+SCAFFOLD = "#"
+ROBOT = "^v<>"
+SCAFFOLD_CHARS = SCAFFOLD + ROBOT
+
+
+class ThoreSubmission(SubmissionPy):
+    def run(self, s):
+        # :param s: input in string format
+        # :return: solution flag
+        program = [int(c) for c in s.split(",")]
+
+        robot = VM(program)
+        robot.run()
+        world_str = "".join(chr(d) for d in robot.stdout).rstrip()
+        # print(world_str)
+
+        world = world_str.splitlines()
+        alignements_sum = 0
+        for i in range(len(world)):
+            for j in range(len(world[0])):
+                if is_intersection(world, i, j):
+                    alignements_sum += i * j
+
+        return alignements_sum
+
+
+def is_intersection(world, i, j):
+    return (
+        (world[i][j] in SCAFFOLD_CHARS)
+        and (i - 1 >= 0 and world[i - 1][j] in SCAFFOLD_CHARS)
+        and (i + 1 < len(world) and world[i + 1][j] in SCAFFOLD_CHARS)
+        and (j - 1 >= 0 and world[i][j - 1] in SCAFFOLD_CHARS)
+        and (j + 1 < len(world) and world[i][j + 1] in SCAFFOLD_CHARS)
+    )
+
+
+def list_to_dict(l):
+    return dict(zip(range(len(l)), l))
+
+
+class VM:
+    def __init__(self, program):
+        self.memory = defaultdict(float, list_to_dict(program))
+        self.pc = 0
+        self.relative_base = 0
+        self.stdin = deque()
+        self.stdout = deque()
+        self.is_halted = False
+        self.is_waiting_for_input = False
+
+        self.opcodes = {
+            1: (self.op_add, 3),
+            2: (self.op_mul, 3),
+            3: (self.op_input, 1),
+            4: (self.op_output, 1),
+            5: (self.op_jump_if_true, 2),
+            6: (self.op_jump_if_false, 2),
+            7: (self.op_lt, 3),
+            8: (self.op_eq, 3),
+            9: (self.op_rel_base, 1),
+            99: (self.op_halt, 0),
+        }
+
+    def add_input(self, value):
+        self.stdin.append(value)
+        self.is_waiting_for_input = False
+
+    def get_output(self):
+        if len(self.stdout) == 0:
+            return None
+        return self.stdout.popleft()
+
+    def get_outputs(self, n=3):
+        if len(self.stdout) < n:
+            return None
+        return [self.stdout.popleft() for i in range(n)]
+
+    def run(self):
+        while not self.is_halted and not self.is_waiting_for_input:
+            self.step()
+
+    def step(self):
+        instruction = self.memory[self.pc]
+        opcode = instruction % 100
+
+        if opcode not in self.opcodes:
+            raise f"Invalid opcode: {opcode}"
+
+        op_func, n_params = self.opcodes[opcode]
+        params = self.get_params_pointers(instruction, n_params)
+        pc = op_func(*params)
+        if pc is not None:
+            self.pc = pc
+
+        return
+
+    def get_params_pointers(self, instruction, n_params):
+        pointers = [None] * n_params
+        instruction //= 100
+        for i in range(n_params):
+            mode = instruction % 10
+            instruction //= 10
+            pointers[i] = self.get_param_pointer(mode, i + 1)
+        return pointers
+
+    def get_param_pointer(self, mode, pos):
+        if mode == 0:
+            return self.memory[self.pc + pos]
+        elif mode == 1:
+            return self.pc + pos
+        elif mode == 2:
+            return self.relative_base + self.memory[self.pc + pos]
+        else:
+            raise ValueError(f"Unknown parameter mode {mode}")
+
+    def op_add(self, a_p, b_p, dest_p):
+        self.memory[dest_p] = self.memory[a_p] + self.memory[b_p]
+        return self.pc + 4
+
+    def op_mul(self, a_p, b_p, dest_p):
+        self.memory[dest_p] = self.memory[a_p] * self.memory[b_p]
+        return self.pc + 4
+
+    def op_input(self, dest_p):
+        if len(self.stdin) == 0:
+            self.is_waiting_for_input = True
+            return None
+        value = self.stdin.popleft()
+        self.memory[dest_p] = value
+        return self.pc + 2
+
+    def op_output(self, a_p):
+        value = self.memory[a_p]
+        self.stdout.append(value)
+        return self.pc + 2
+
+    def op_jump_if_true(self, cond_p, value_p):
+        if self.memory[cond_p] != 0:
+            return self.memory[value_p]
+        else:
+            return self.pc + 3
+
+    def op_jump_if_false(self, cond_p, value_p):
+        if self.memory[cond_p] == 0:
+            return self.memory[value_p]
+        else:
+            return self.pc + 3
+
+    def op_lt(self, a_p, b_p, dest_p):
+        self.memory[dest_p] = int(self.memory[a_p] < self.memory[b_p])
+        return self.pc + 4
+
+    def op_eq(self, a_p, b_p, dest_p):
+        self.memory[dest_p] = int(self.memory[a_p] == self.memory[b_p])
+        return self.pc + 4
+
+    def op_rel_base(self, val_p):
+        self.relative_base += self.memory[val_p]
+        return self.pc + 2
+
+    def op_halt(self):
+        self.is_halted = True
+        return None
+
