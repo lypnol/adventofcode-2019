37be5d3aff253729fe40910025165e9d89af9571
Francisco-Pierre Puig <puig.franciscopierre@gmail.com>
2019-12-17 12:07:25 +0100

    francisco: faster day 16

--- day-16/part-2/francisco.py
+++ day-16/part-2/francisco.py
@@ -1,18 +1,31 @@
 from tool.runners.python import SubmissionPy
 
+import itertools
+
+import numpy as np
+
 
 def fft_iter_2(l, offset, times=100):
     # Key assumption for part 2
     assert offset > 10000 * len(l) // 2
-    l = [l[i % len(l)] for i in range(offset, 10000 * len(l))]
+    # l = np.array([l[i % len(l)] for i in range(offset, 10_000 * len(l))])
+    l = np.array(
+        list(
+            itertools.islice(
+                itertools.chain(l[offset % len(l) :], itertools.cycle(l)),
+                0,
+                10000 * len(l) - offset,
+            )
+        )
+    )
+    l = np.flip(l)
     for i in range(times):
         # simplified ftt computation using linear cumulative sum algorithm, because we
         # have:
         #   fft[i] = sum(l[i] for i in range(i, len(l)))
-        for i in reversed(range(len(l) - 1)):
-            l[i] += l[i + 1]
-        for i in reversed(range(len(l) - 1)):
-            l[i] = abs(l[i]) % 10
+        np.cumsum(l, out=l)
+        np.mod(l, 10, out=l)
+    l = np.flip(l)
     return l
 
 
