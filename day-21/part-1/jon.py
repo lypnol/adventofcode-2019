36131e34a5789fd3c96b286327f3a46215aac8c1
Jonathan Remy <jonremy@users.noreply.github.com>
2019-12-23 23:43:12 +0100

    Add jon day 21

---
+++ day-21/part-1/jon.py
@@ -0,0 +1,79 @@
+from tool.runners.python import SubmissionPy
+import collections
+
+
+class JonSubmission(SubmissionPy):
+
+    def run(self, s):
+        code = [int(v) for v in s.strip().split(",")]
+
+        program = """
+NOT A J
+NOT B T
+OR T J
+NOT C T
+OR T J
+AND D J
+WALK
+"""
+
+        inputs = [ord(c) for c in program.lstrip()]
+
+        for v in compute(code, inputs):
+            if v > 127:
+                return v
+            # print(chr(v), end="")
+
+        return -1
+
+
+def compute(code, inputs):
+    p = collections.defaultdict(int, enumerate(code))
+    pc = 0
+    relative_base = 0
+
+    def index(pos):
+        mode = (p[pc] // 10 ** (1 + pos)) % 10
+        if mode == 0:
+            return p[pc + pos]
+        if mode == 1:
+            return pc + pos
+        if mode == 2:
+            return relative_base + p[pc + pos]
+        raise Exception("Bad param mode")
+
+    input_index = 0
+
+    while True:
+        op = p[pc] % 100
+
+        if op == 1:
+            p[index(3)] = p[index(1)] + p[index(2)]
+            pc += 4
+        elif op == 2:
+            p[index(3)] = p[index(1)] * p[index(2)]
+            pc += 4
+        elif op == 3:
+            p[index(1)] = inputs[input_index]
+            input_index += 1
+            pc += 2
+        elif op == 4:
+            yield p[index(1)]
+            pc += 2
+        elif op == 5:
+            pc = p[index(2)] if p[index(1)] != 0 else pc + 3
+        elif op == 6:
+            pc = p[index(2)] if p[index(1)] == 0 else pc + 3
+        elif op == 7:
+            p[index(3)] = 1 if p[index(1)] < p[index(2)] else 0
+            pc += 4
+        elif op == 8:
+            p[index(3)] = 1 if p[index(1)] == p[index(2)] else 0
+            pc += 4
+        elif op == 9:
+            relative_base += p[index(1)]
+            pc += 2
+        elif op == 99:
+            break
+        else:
+            raise Exception("Unknown op code")
