d8c92fa5a46825b35b1bbdad89642aa2eb8d9350
Francisco-Pierre Puig <puig.franciscopierre@gmail.com>
2019-12-23 20:09:32 +0100

    francisco: better day 22

--- day-22/part-2/francisco.py
+++ day-22/part-2/francisco.py
@@ -3,6 +3,7 @@ from tool.runners.python import SubmissionPy
 
 import math
 import re
+import time
 
 DEAL_INCR, NEW_STACK, CUT = 0, 1, 2
 RE = {
@@ -64,85 +65,73 @@ def euclid(a, b):
     return y, x - q * y, gcd
 
 
+def operation_to_affine_transformation(operation, length):
+    kind = operation[0]
+    if kind == NEW_STACK:
+        return (-1, length - 1)
+    elif kind == CUT:
+        cut = operation[1]
+        return (1, -cut)
+    elif kind == DEAL_INCR:
+        incr = operation[1]
+        return (incr, 0)
+    else:
+        assert False
+
+
+def merge_affine_transformations(affine_transformations, length):
+    a, b = 1, 0
+    for (a1, b1) in affine_transformations:
+        a = (a * a1) % length
+        b = (a1 * b + b1) % length
+    return a, b
+
+
+def solve_part1_alt(operations, length=10007):
+    a, b = merge_affine_transformations(
+        [
+            operation_to_affine_transformation(operation, length)
+            for operation in operations
+        ],
+        length,
+    )
+    return (a * 2019 + b) % length
+
+
 def inverse_modulo(i, p):
     x, y, gcd = euclid(i, p)
     assert gcd == 1, f"i and p not coprimes (i={i} p={p} gcd={gcd})"
     return x
 
 
-def prev_position(position, length, operation):
-    kind = operation[0]
-    if kind == NEW_STACK:
-        return length - 1 - position
-    elif kind == CUT:
-        cut = operation[1]
-        return (position + cut) % length
-    elif kind == DEAL_INCR:
-        incr = operation[1]
-        return (position * inverse_modulo(incr, length)) % length
-    else:
-        assert False
-
-
-def apply_operations_alt(position, length, operations):
-    for operation in reversed(operations):
-        position = prev_position(position, length, operation)
-    return position
-
-
-def solve_part1_alt(operations, l=10007):
-    # This method is slower, but it allowed me to check that my prev_position function
-    # was correct.
-    deck = list(range(l))
-    deck = [apply_operations_alt(position, l, operations) for position in deck]
-    return deck.index(2019)
-
-
-def convert_operation(operation, length):
-    kind = operation[0]
-    if kind == NEW_STACK:
-        return (-1, length - 1)
-    elif kind == CUT:
-        cut = operation[1]
-        return (1, cut)
-    elif kind == DEAL_INCR:
-        incr = operation[1]
-        return (inverse_modulo(incr, length), 0)
-    else:
-        assert False
-
-
-def stack_operations(converted_operations, length):
-    a, b = 1, 0
-    for (a1, b1) in reversed(converted_operations):
-        a = (a * a1) % length
-        b = (a1 * b + b1) % length
-    return a, b
-
-
-def solve_part1_alt_alt(operations, length=10007):
-    # This method is faster than the naive one ! and it allowed me to check that my
-    # stack_operations and convert_operation functions were working.
-    a, b = stack_operations(
-        [convert_operation(operation, length) for operation in operations], length
-    )
-    deck = list(range(length))
-    deck = [(a * p + b) % length for p in deck]
-    return deck.index(2019)
+def inverse_affine_transformation(a, b, length):
+    a_ = inverse_modulo(a, length)
+    return a_, (-a_ * b) % length
 
 
 def solve_part2(operations, length=119_315_717_514_047, times=101_741_582_076_661):
     position = 2020
-    a, b = stack_operations(
-        [convert_operation(operation, length) for operation in operations], length
+    a, b = merge_affine_transformations(
+        [
+            operation_to_affine_transformation(operation, length)
+            for operation in operations
+        ],
+        length,
     )
+    a, b = inverse_affine_transformation(a, b, length)
+
+    a_total, b_total = 1, 0
 
     while times > 0:
         if times & 1 == 1:
-            position = (a * position + b) % length
+            a_total, b_total = merge_affine_transformations(
+                [(a, b), (a_total, b_total)], length
+            )
+
         times >>= 1
-        a, b = stack_operations([(a, b)] * 2, length)
-    return position
+        a, b = merge_affine_transformations([(a, b)] * 2, length)
+
+    return (a_total * position + b_total) % length
 
 
 class FranciscoSubmission(SubmissionPy):
