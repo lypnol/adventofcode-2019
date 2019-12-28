ac4c8ef8870bf44cc43fc0759d91766903da2d42
Younes Belkouchi <belkouchiyounes@gmail.com>
2019-12-28 19:01:01 +0100

    youyoun day 16 p 2

--- day-16/part-2/youyoun.py
+++ day-16/part-2/youyoun.py
@@ -1,28 +1,9 @@
 from tool.runners.python import SubmissionPy
+import itertools
 import numpy as np
 
-BASE_PATTERN = [0, 1, 0, -1]
 FINAL_PHASE = 100
-PATTERNS = []
-
-
-def memoize_pattern(pattern):
-    PATTERNS.append(pattern)
-    return
-
-
-def get_pattern(n_digit, len_):
-    if n_digit < len(PATTERNS):
-        return PATTERNS[n_digit]
-    pat = []
-    i = 0
-    while len(pat) < len_ + 1:
-        for _ in range(n_digit + 1):
-            pat.append(BASE_PATTERN[i])
-        i = (i + 1) % len(BASE_PATTERN)
-    pat = np.array(pat[1:len_ + 1])
-    memoize_pattern(pat)
-    return pat
+REPS = 10000
 
 
 class YouyounSubmission(SubmissionPy):
@@ -31,11 +12,20 @@ class YouyounSubmission(SubmissionPy):
         # :param s: input in string format
         # :return: solution flag
         # Your code goes here
-        input_ = np.array([int(x) for x in s])
+        input_ = [int(x) for x in s]
+        offset = int(s[:7])
         n = len(input_)
-        for i in range(n):
-            _ = get_pattern(i, n)
-        pats = np.array(PATTERNS)
+        input_ = np.flip(
+            np.array(
+                list(itertools.islice(
+                    itertools.chain(itertools.islice(input_, offset % n, n), itertools.cycle(input_)),
+                    0,
+                    n * REPS - offset,
+                ))
+            )
+        )
         for phase in range(FINAL_PHASE):
-            input_ = np.mod(np.abs(pats @ input_), 10)
-        return "".join([str(input_[i]) for i in range(8)])
+            input_ = np.cumsum(input_)
+            if phase % 3 == 0:
+                input_ = np.mod(input_, 10)
+        return "".join([str(x) for x in input_[:-9:-1]])
